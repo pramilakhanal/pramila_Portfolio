2f3db30275b4dc8e7fdd416a2fa4db1a549d5cbb
pramila khanal <pramilakhanal21@gmail.com>
2018-05-08 11:52:25 -0600

    new portfolio

---
+++ js/init.js
@@ -0,0 +1,34 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+//   }); // end of document ready
+// })(jQuery); // end of jQuery name space
+
+   $(document).ready(function(){
+      //   $('.carousel').carousel();
+      // });
+
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+ });
+
+//     // Next slide
+//   $('.carousel').carousel('next');
+//  $('.carousel').carousel('next', 3); // Move next n times.
+
+//   // Previous slide
+//   $('.carousel').carousel('prev');
+//  $('.carousel').carousel('prev', 4); // Move prev n times.
+
+//   // Set to nth slide
+//   $('.carousel').carousel('set', 4);
+
+// // Destroy carousel
+//   $('.carousel').carousel('destroy');
+
+  
+
+      }); // end of document ready
+})(jQuery);
